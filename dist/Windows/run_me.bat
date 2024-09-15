fc3258108cbbbd4a808dc2a919f9a57ec0f8741c
jaudo <carlosagorreta@gmail.com>
2024-09-15 15:26:32 +0200

    Add files via upload

---
+++ dist/Windows/run_me.bat
@@ -0,0 +1,36 @@
+@echo off
+REM Path to your first executable
+set EXE_PATH1=".\dut1.exe"
+
+REM Path to your first executable
+set EXE_PATH2=".\dut2.exe"
+
+REM Path to your first executable
+set EXE_PATH3=".\dut3.exe"
+
+REM Path to your first executable
+set EXE_PATH4=".\dut4.exe"
+
+REM Path to your first executable
+set EXE_PATH5=".\dut5.exe"
+
+REM Path to your first executable
+set EXE_PATH6=".\dut6.exe"
+
+REM Start the first executable in a new window
+start "" cmd /c %EXE_PATH1%
+
+REM Start the second executable in a new window
+start "" cmd /c %EXE_PATH2%
+
+REM Start the third executable in a new window
+start "" cmd /c %EXE_PATH3%
+
+REM Start the third executable in a new window
+start "" cmd /c %EXE_PATH4%
+
+REM Start the third executable in a new window
+start "" cmd /c %EXE_PATH5%
+
+REM Start the third executable in a new window
+start "" cmd /c %EXE_PATH6%
